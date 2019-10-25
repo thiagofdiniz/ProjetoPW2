d226e0832d1c7158e9a263a7075fc5cc6b8725ed
thiagofdiniz <thiago.diniz2@etec.sp.gov.br>
2019-10-24 22:14:07 -0300

    add do menu (em teoria)

--- projetopw.sql
+++ projetopw.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 04-Out-2019 às 17:15
--- Versão do servidor: 10.1.35-MariaDB
--- versão do PHP: 7.2.9
+-- Generation Time: 25-Out-2019 às 03:13
+-- Versão do servidor: 5.7.21-log
+-- versão do PHP: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,6 +25,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `alternativas`
+--
+
+CREATE TABLE `alternativas` (
+  `idAlternativa` int(11) NOT NULL,
+  `texto` varchar(200) NOT NULL,
+  `idQuestao` int(11) NOT NULL,
+  `correta` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `questoes`
 --
 
@@ -33,6 +46,13 @@ CREATE TABLE `questoes` (
   `pergunta` varchar(200) NOT NULL,
   `tipo` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `questoes`
+--
+
+INSERT INTO `questoes` (`idPergunta`, `pergunta`, `tipo`) VALUES
+(9, '123', 'abc');
 
 -- --------------------------------------------------------
 
@@ -52,12 +72,18 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`idUsuario`, `nome`, `email`, `senha`) VALUES
-(0, 'Thiago', '123@gmail.com', '202cb962ac59075b964b07152d234b70'),
-(8, 'Vanessa', '1234@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055');
+(0, 'Thiago', '123@gmail.com', '202cb962ac59075b964b07152d234b70');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `alternativas`
+--
+ALTER TABLE `alternativas`
+  ADD PRIMARY KEY (`idAlternativa`),
+  ADD KEY `alternativa_questao` (`idQuestao`);
 
 --
 -- Indexes for table `questoes`
@@ -77,16 +103,32 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT for table `alternativas`
+--
+ALTER TABLE `alternativas`
+  MODIFY `idAlternativa` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `questoes`
 --
 ALTER TABLE `questoes`
-  MODIFY `idPergunta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `idPergunta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
   MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `alternativas`
+--
+ALTER TABLE `alternativas`
+  ADD CONSTRAINT `alternativa_questao` FOREIGN KEY (`idQuestao`) REFERENCES `questoes` (`idPergunta`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
