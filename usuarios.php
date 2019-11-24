c83609316cc2abf65d1d560d25a991234626e95f
thiagofdiniz <thiago.diniz2@etec.sp.gov.br>
2019-11-24 00:57:07 -0300

    up

--- usuarios.php
+++ usuarios.php
@@ -1,7 +1,7 @@
 <?php
 include "UsuarioDAO.php";
 include "alertas.php";
-include "verificarLogin";
+include "verificarLogin.php";
 
 $usuarioDAO = new UsuarioDAO();
 $lista = $usuarioDAO->buscar();
