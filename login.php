c273fe8afbd84d5f7643e1b7c3f6005f4efdfd0e
thiagofdiniz <thiago.diniz2@etec.sp.gov.br>
2019-11-24 00:54:01 -0300

    up

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
 
 <body>
 	<div id="login">
-    <h3 class="text-center text-white pt-5">Login form</h3>
+    <h3 class="text-center text-black pt-5">Login form</h3>
     <div class="container">
         <div id="login-row" class="row justify-content-center align-items-center">
             <div id="login-column" class="col-md-6">
@@ -17,16 +17,14 @@
                             <input type="text" name="email" id="username" class="form-control">
                         </div>
                         <div class="form-group">
-                            <label for="password" class="text-info">Password:</label><br>
+                            <label for="password" class="text-info">Senha:</label><br>
                             <input type="text" name="senha" id="password" class="form-control">
                         </div>
                         <div class="form-group">
-                            <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
-                            <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
+                            
+                            <input type="submit" name="submit" class="btn btn-info btn-md" value="Entrar">
                         </div>
-                        <div id="register-link" class="text-right">
-                            <a href="#" class="text-info">Register here</a>
-                        </div>
+                        
                     </form>
                 </div>
             </div>
